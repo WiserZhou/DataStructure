6c6f8ff7b6d826794b516ff9a2c1919087eb34e8
WiserZhou <wiserx@qq.com>
2023-10-05 20:29:09 +0800

    稀疏矩阵乘法的简单算法

--- DataStructure/header/SparseMatrix.h
+++ DataStructure/header/SparseMatrix.h
@@ -71,53 +71,54 @@ Status FastTransposeSMatrix(TS_Matrix M, TS_Matrix &T)
     }
     return OK;
 }
+
 #define MAX_RC 100
 typedef struct
 {
     Triple data[MAX_SIZE + 1];
-    int r_pos[MAX_RC + 1];
+    int r_pos[MAX_RC + 1]; // 各行第一个非零元的位置表
     int mu, nu, tu;
 } RLS_Matrix;
 
 Status MultSMatrix(RLS_Matrix M, RLS_Matrix N, RLS_Matrix &Q)
 {
-    if (M.nu != N.mu)
+    if (M.nu != N.mu) // 不匹配则无法相乘
         return ERROR;
 
     Q.mu = M.mu;
     Q.nu = N.nu;
-    Q.tu = 0;
+    Q.tu = 0; // 设置乘积矩阵的属性
 
-    if (M.tu * N.tu != 0)
+    if (M.tu * N.tu != 0) // 如果一个是零矩阵，则没必要计算
     {
-        for (int arrow = 1; arrow <= M.mu; ++arrow)
+        for (int arrow = 1; arrow <= M.mu; ++arrow) // 利用M的行数进行乘法的运算
         {
             int c_temp[MAX_RC] = {0};
-            Q.r_pos[arrow] = Q.tu + 1;
+            Q.r_pos[arrow] = Q.tu + 1; // 初始化这一行的第一个元素的位置
             int tp;
             if (arrow < M.mu)
-                tp = M.r_pos[arrow + 1];
+                tp = M.r_pos[arrow + 1]; // 获得这一行最后一个元素位置的下一个，作为终止条件
             else
-                tp = M.tu + 1;
-            for (int p = M.r_pos[arrow]; p < tp; ++p)
+                tp = M.tu + 1;                        // 最后一行的终止条件比较特殊
+            for (int p = M.r_pos[arrow]; p < tp; ++p) // 遍历这一行所有的元素位置
             {
-                int brow = M.data[p].j;
+                int brow = M.data[p].j; // 得到M元素的列值
                 int t;
-                if (brow < N.mu)
+                if (brow < N.mu) // 同样得到N行的终止条件
                     t = N.r_pos[brow + 1];
                 else
                     t = N.tu + 1;
-                for (int q = N.r_pos[brow]; q < t; q++)
+                for (int q = N.r_pos[brow]; q < t; q++) // 同时遍历N行的全部元素
                 {
-                    int c_col = N.data[q].j;
-                    c_temp[c_col] += M.data[p].e * N.data[q].e;
+                    int c_col = N.data[q].j;                    // 得到N的元素的列值
+                    c_temp[c_col] += M.data[p].e * N.data[q].e; // 计算在此列的乘积计算值
                 }
             }
-            for (int c_col = 1; c_col <= Q.nu; ++c_col)
+            for (int c_col = 1; c_col <= Q.nu; ++c_col) // 遍历Q的所有列
             {
-                if (c_temp[c_col])
+                if (c_temp[c_col]) // 如果有值
                 {
-                    if (++Q.tu > MAX_SIZE)
+                    if (++Q.tu > MAX_SIZE) // 如果超过最大范围，因为稀疏矩阵的乘积不一定也是稀疏矩阵
                         return ERROR;
                     Q.data[Q.tu].i = arrow;
                     Q.data[Q.tu].j = c_col;
