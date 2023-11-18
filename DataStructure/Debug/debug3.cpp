9a6a62d2ec49f412ddf3ec0dcae4b0f2675e3fb1
WiserZhou <wiserx@qq.com>
2023-11-18 14:44:23 +0800

    实验题

--- DataStructure/Debug/debug3.cpp
+++ DataStructure/Debug/debug3.cpp
@@ -1,25 +1,48 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
+#include <vector>
+#include <iomanip>
 using namespace std;
-struct Node
+
+void search(vector<vector<int>> matrix, int depth, int &num, vector<int> &visit, int index)
 {
-    char ch;            // 字符
-    int freq;           // 频率
-    Node *left, *right; // 左右子结点
-    Node(char c, int f) : ch(c), freq(f), left(nullptr), right(nullptr) {}
-};
-int main()
-{
-    string str;
-    unordered_map<char, int> alphaSet;
-    getline(cin, str);
-    for (char c : str)
+    if (depth >= 6)
+        return;
+    for (int i = 0; i < matrix.size(); i++)
     {
-        if (isalpha(c))
+        if (visit[i] == 0 && matrix[index][i] == 1)
         {
-            ++alphaSet[c];
+            num++;
+            visit[i] = 1;
+            search(matrix, depth + 1, num, visit, i);
         }
     }
-    
+}
+
+int main()
+{
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> matrix(N, vector<int>(N, 0));
+
+    for (int i = 0; i < M; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        matrix[x - 1][y - 1] = 1;
+        matrix[y - 1][x - 1] = 1;
+    }
+    // cout << "OK";
+    for (int i = 0; i < N; i++)
+    {
+        vector<int> visit(N, 0);
+
+        visit[i] = 1;
+        int num = 1;
+        search(matrix, 0, num, visit, i);
+        // cout << num << endl;
+        cout << fixed << setprecision(2) << i + 1 << ": " << num / (double)N * 100 << "%" << endl;
+    }
+    return 0;
 }
